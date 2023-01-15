17f0a9160b57db3b6181a90a2f695a57a0a9070d
Paul Sterl <paul@sterl.org>
2023-01-15 10:54:20 +0100

    move datasource code into own class

---
+++ src/app/bpmn/page/browse-digram-page/browse-diagram-data-source.ts
@@ -0,0 +1,120 @@
+import { FlatTreeControl } from "@angular/cdk/tree";
+import { MatTreeFlatDataSource, MatTreeFlattener } from "@angular/material/tree";
+import { BpmnElement, BpmnType } from "../../model/diagram-model";
+import { AppDbService } from "../../service/app-db.service";
+import { BpmnDiagramService } from "../../service/bpmn-diagram.service";
+import { BpmnElementService } from "../../service/bpmn-element.service";
+
+export interface FlatTreeNode {
+    name: string;
+    type: BpmnType;
+    level: number;
+    expandable: boolean;
+    source: BpmnElement;
+    updateDate?: number;
+}
+
+export class BrowseDiagramDataSource {
+
+  /** The TreeControl controls the expand/collapse state of tree nodes.  */
+  treeControl: FlatTreeControl<FlatTreeNode>;
+
+  /** The TreeFlattener is used to generate the flat list of items from hierarchical data. */
+  treeFlattener: MatTreeFlattener<BpmnElement, FlatTreeNode>;
+
+  /** The MatTreeFlatDataSource connects the control and flattener to provide data. */
+  dataSource: MatTreeFlatDataSource<BpmnElement, FlatTreeNode>;
+
+  public readonly flatTreeNodeCache = new Map<BpmnElement, FlatTreeNode>();
+  private readonly elementMap = new Map<number, BpmnElement>();
+
+
+  constructor(
+    private elementService: BpmnElementService, 
+    private diagramService: BpmnDiagramService, 
+    private db: AppDbService) {
+
+    this.treeFlattener = new MatTreeFlattener(
+      (n, l) => this._nodeTransformer(n, l),
+      n => n.level,
+      n => n.expandable,
+      n => n.children ? n.children : null
+    );
+
+    this.treeControl = new FlatTreeControl(n => n.level, n => n.expandable);
+    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+  }
+
+  remove(element: BpmnElement) {
+    this.dataSource.data = this.dataSource.data.filter(el => el != element || el.id !== element.id);
+  }
+
+  /** Transform the data to something the tree can read. */
+  private _nodeTransformer(node: BpmnElement, level: number): FlatTreeNode {
+    let r = this.flatTreeNodeCache.get(node);
+    if (!r) {
+      r = {
+        name: node.name,
+        type: node.type,
+        level,
+        expandable: node.children != undefined && node.children.length > 0,
+        updateDate: node.updateDate,
+        source: node
+      };
+      this.flatTreeNodeCache.set(node, r);
+    }
+    return r;
+  }
+
+  removeElementsWithoutId() {
+    const d = this.dataSource.data.filter(el => el.id !== undefined);
+    this.dataSource.data = [];
+    this.dataSource.data = d;
+  }
+
+  rerenderAllElements() {
+    const d = this.dataSource.data;
+    this.dataSource.data = [];
+    this.dataSource.data = d;
+  }
+
+  async doReload(): Promise<void> {
+    this.flatTreeNodeCache.clear();
+    this.elementMap.clear();
+
+    const elements = await this.elementService.findAll();
+    const indexMap = new Map<number, BpmnElement>();
+    const rootElements = new Array<BpmnElement>();
+  
+    // First pass: identify root elements and create element map
+    for (const e of elements) {
+      e.children = [];
+      indexMap.set(e.id!, e);
+      if (e.parentId == 0) {
+        rootElements.push(e);
+      }
+    }
+  
+    // Second pass: process children of each element
+    for (const e of elements) {
+      if (e.parentId && e.parentId != 0) {
+        indexMap.get(e.parentId)?.children?.push(e);
+      }
+    }
+
+    const openState = this.treeControl.expansionModel.selected;
+    this.dataSource.data = rootElements;
+    if (openState.length > 0) {
+      this._expandById(openState.map(n => n.source.id!));
+    } else {
+      const lastElement = await this.elementService.getLastChangedElement();
+      this._expandById(lastElement.filter(l => l.parentId).map(l => l.parentId!));
+    }
+  }
+
+  private _expandById(ids: Array<number>) {
+    for (let n of this.flatTreeNodeCache.values()) {
+      if (n.source.id && ids.includes(n.source.id)) this.treeControl.expand(n);
+    }
+  }
+}
--- src/app/bpmn/page/browse-digram-page/browse-digram-page.component.ts
+++ src/app/bpmn/page/browse-digram-page/browse-digram-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
-import { FlatTreeControl } from '@angular/cdk/tree';
 import { BpmnElement, BpmnType, newElement } from '../../model/diagram-model';
 import { BpmnElementService } from '../../service/bpmn-element.service';
 import { BpmnDiagramService } from '../../service/bpmn-diagram.service';
@@ -8,20 +6,10 @@ import { AppDbService } from '../../service/app-db.service';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent, ConfirmDialogModel } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
+import { BrowseDiagramDataSource, FlatTreeNode } from './browse-diagram-data-source';
 
 
-/**
- * Flattened tree node that has been created from a FileNode through the flattener. Flattened
- * nodes include level index and whether they can be expanded or not.
- */
-interface FlatTreeNode {
-  name: string;
-  type: BpmnType;
-  level: number;
-  expandable: boolean;
-  source: BpmnElement;
-  updateDate?: number;
-}
+
 
 @Component({
   selector: 'app-browse-digram-page',
@@ -32,20 +20,10 @@ export class BrowseDigramPageComponent implements OnInit {
 
   @ViewChild('inputName') inputName?: ElementRef;
 
-  /** The TreeControl controls the expand/collapse state of tree nodes.  */
-  treeControl: FlatTreeControl<FlatTreeNode>;
-
-  /** The TreeFlattener is used to generate the flat list of items from hierarchical data. */
-  treeFlattener: MatTreeFlattener<BpmnElement, FlatTreeNode>;
-
-  /** The MatTreeFlatDataSource connects the control and flattener to provide data. */
-  dataSource: MatTreeFlatDataSource<BpmnElement, FlatTreeNode>;
-
-  private readonly flatTreeNodeCache = new Map<BpmnElement, FlatTreeNode>();
-  private readonly elementMap = new Map<number, BpmnElement>();
-
   editElement?: BpmnElement;
   parent?: BpmnElement;
+
+  ds: BrowseDiagramDataSource;
 
   constructor(
     private dialog: MatDialog,
@@ -54,19 +32,11 @@ export class BrowseDigramPageComponent implements OnInit {
     private diagramService: BpmnDiagramService, 
     private db: AppDbService) {
 
-    this.treeFlattener = new MatTreeFlattener(
-      (n, l) => this._nodeTransformer(n, l),
-      n => n.level,
-      n => n.expandable,
-      n => n.children ? n.children : null
-    );
-
-    this.treeControl = new FlatTreeControl(n => n.level, n => n.expandable);
-    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+    this.ds = new BrowseDiagramDataSource(elementService, diagramService, db);
   }
 
   ngOnInit() {
-    this.doReload();
+    this.ds.doReload();
   }
 
   cancelEdit = () => {
@@ -75,11 +45,9 @@ export class BrowseDigramPageComponent implements OnInit {
       && this.parent.children!.indexOf(this.editElement) >= 0) {      
       this.parent.children!.splice(this.parent.children!.indexOf(this.editElement), 1);
     }
-    const d = this.dataSource.data.filter(el => el.id !== undefined);
     this.parent = undefined;
     this.editElement = undefined;
-    this.dataSource.data = [];
-    this.dataSource.data = d;
+    this.ds.removeElementsWithoutId();
     
   };
 
@@ -100,9 +68,7 @@ export class BrowseDigramPageComponent implements OnInit {
       this.router.navigate(['/diagrams', node.source.id || 'new']);
     } else {
       this.editElement = node.source;
-      const d = this.dataSource.data;
-      this.dataSource.data = [];
-      this.dataSource.data = d;
+      this.ds.rerenderAllElements();
       this._focusInput();
     }
   }
@@ -114,10 +80,10 @@ export class BrowseDigramPageComponent implements OnInit {
       this.parent = node.source;
       this.parent.children!.push(folder);
       node.expandable = true;
-      this.dataSource.data = this.dataSource.data;
-      this.treeControl.expand(node);
+      this.ds.dataSource.data = this.ds.dataSource.data;
+      this.ds.treeControl.expand(node);
     } else {      
-      this.dataSource.data = new Array<BpmnElement>(folder).concat(this.dataSource.data);
+      this.ds.dataSource.data = new Array<BpmnElement>(folder).concat(this.ds.dataSource.data);
     }
     this._focusInput();
   }
@@ -137,52 +103,12 @@ export class BrowseDigramPageComponent implements OnInit {
   private async _deleteElement(element: BpmnElement): Promise<void> {
     await this.diagramService.delete(element.id);
     await this.elementService.delete(element.id);
-    this.flatTreeNodeCache.delete(element);
+    this.ds.flatTreeNodeCache.delete(element);
     // if we have children on the deleted node, we have to reload
     if (element.children?.length || 0 > 0) {
-      this.doReload();
+      this.ds.doReload();
     } else {
-      this.dataSource.data = this.dataSource.data.filter(el => el != element || el.id !== element.id);
-    }
-  }
-
-  async doReload(): Promise<void> {
-    this.flatTreeNodeCache.clear();
-    this.elementMap.clear();
-
-    const elements = await this.elementService.findAll();
-    const indexMap = new Map<number, BpmnElement>();
-    const rootElements = new Array<BpmnElement>();
-  
-    // First pass: identify root elements and create element map
-    for (const e of elements) {
-      e.children = [];
-      indexMap.set(e.id!, e);
-      if (e.parentId == 0) {
-        rootElements.push(e);
-      }
-    }
-  
-    // Second pass: process children of each element
-    for (const e of elements) {
-      if (e.parentId && e.parentId != 0) {
-        indexMap.get(e.parentId)?.children?.push(e);
-      }
-    }
-
-    const openState = this.treeControl.expansionModel.selected;
-    this.dataSource.data = rootElements;
-    if (openState.length > 0) {
-      this.expandById(openState.map(n => n.source.id!));
-    } else {
-      const lastElement = await this.elementService.getLastChangedElement();
-      this.expandById(lastElement.filter(l => l.parentId).map(l => l.parentId!));
-    }
-  }
-
-  expandById(ids: Array<number>) {
-    for (let n of this.flatTreeNodeCache.values()) {
-      if (n.source.id && ids.includes(n.source.id)) this.treeControl.expand(n);
+      this.ds.remove(element);
     }
   }
 
@@ -190,7 +116,7 @@ export class BrowseDigramPageComponent implements OnInit {
     if (this.editElement && this.editElement.name && this.editElement.name.length > 0) {
       await this.elementService.save(this.editElement);
       this.editElement = undefined;
-      await this.doReload();
+      await this.ds.doReload();
     }
   }
 
@@ -198,30 +124,13 @@ export class BrowseDigramPageComponent implements OnInit {
   isEmptyFolder = (_: number, node: FlatTreeNode) => !node.expandable && node.source && node.source.type === BpmnType.folder;
   isFolder = (node?: FlatTreeNode) => node &&  node.type === BpmnType.folder;
 
-  /** Transform the data to something the tree can read. */
-  private _nodeTransformer(node: BpmnElement, level: number): FlatTreeNode {
-    let r = this.flatTreeNodeCache.get(node);
-    if (!r) {
-      r = {
-        name: node.name,
-        type: node.type,
-        level,
-        expandable: node.children != undefined && node.children.length > 0,
-        updateDate: node.updateDate,
-        source: node
-      };
-      this.flatTreeNodeCache.set(node, r);
-    }
-    return r;
-  }
-
   async doClear() {
     this.dialog.open(ConfirmDialogComponent, {
       data: new ConfirmDialogModel(`Do your really want to delete all data? Everything will be removed.`, 'Confirm clear data')}
     ).afterClosed().subscribe(async (okay) => {
       if (okay) {
         await this.db.clear();
-        await this.doReload();
+        await this.ds.doReload();
       }
     });
     
