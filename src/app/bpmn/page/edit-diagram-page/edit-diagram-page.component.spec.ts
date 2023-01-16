2ca843ca17b848a600069fb1456c81512519dd8a
Paul Sterl <paul@sterl.org>
2023-01-16 17:01:16 +0100

    using menu also in editor

--- src/app/bpmn/page/edit-diagram-page/edit-diagram-page.component.spec.ts
+++ src/app/bpmn/page/edit-diagram-page/edit-diagram-page.component.spec.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -29,7 +30,8 @@ describe('EditDiagramPageComponent', () => {
         MatButtonModule,
         MatIconModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatMenuModule
       ]
     })
     .compileComponents();
